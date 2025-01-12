0c4e67c381fe39d567581bcb8dada9b88aad59e7
Fabri <ism.fabri@gmail.com>
2025-01-12 03:37:01 -0300

    Remover arquivos deletados do projeto

--- eLivre/core/loja/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('lancamentos/', views.lancamentos, name='lancamentos'),
-    path('masculino/', views.masculino, name='masculino'),
-    path('feminino/', views.feminino, name='feminino'),
-]
